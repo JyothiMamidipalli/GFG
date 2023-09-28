03cb44e6a9148867b36f0642365510c6dad117d5
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2023-09-29 00:08:21 +0530

    Added Solution - GfG to GitHub

---
+++ School/Count Digits/count-digits.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        int cnt=0;
+        int num=N;
+        while(num!=0)
+        {
+            int temp=num%10;
+            if(temp!=0 && N%temp==0) cnt++;
+            num/=10;
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
