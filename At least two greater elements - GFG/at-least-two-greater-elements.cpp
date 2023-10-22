e28d1a8ea7c4e130e238733fe28d3dcca56924c6
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2023-10-22 11:31:19 +0530

    Added solution - LeetHub

---
+++ At least two greater elements - GFG/at-least-two-greater-elements.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+public:
+    vector<int> findElements(int a[], int n)
+    {
+        // Your code goes here
+        vector<int>raj;
+        for(int i=0;i<n;i++)
+        {
+            raj.push_back(a[i]);
+        }
+        sort(raj.begin(),raj.end());
+        raj.pop_back();
+        raj.pop_back();
+        return raj;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        cin>>a[i];
+        Solution ob;
+        vector <int> res = ob.findElements(a,n);
+        
+        for(int i=0;i<res.size();i++)
+        cout<<res[i]<<" ";
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
