10d6a879144e90711872a1867525e65b2e795371
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2023-11-05 22:27:00 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Binary Search/binary-search.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int bin=binary_search(arr,arr+n,k);
+        if(!bin) return -1;
+        auto low=lower_bound(arr,arr+n,k);
+        int l=low-arr;
+        return l;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++) cin >> arr[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        int found = ob.binarysearch(arr, N, key);
+        cout << found << endl;
+    }
+}
+
+// } Driver Code Ends
