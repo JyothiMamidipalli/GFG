d99a5745a2bfb3650d789cdfe218968c0b7f43de
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2023-12-04 17:41:26 +0530

    Added Solution - GfG to GitHub

---
+++ School/Find n-th term of series 1, 3, 6, 10, 15, 21/find-nth-term-of-series-1-3-6-10-15-21.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findNthTerm(int N) {
+        // code here
+        int sum = 0;
+      for ( int i = 1; i <= N; i++){
+          sum += i;
+      }
+      return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.findNthTerm(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
