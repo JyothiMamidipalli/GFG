ce37eea67c922751bb50050abd6ed17ec058ef15
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2023-12-06 11:44:50 +0530

    Added Solution - GfG to GitHub

---
+++ School/C++ Operators (Relational) | Set 2/c-operators-relational-set-2.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+class Solution{   
+public:
+    string compareNum(int A,int B){
+        // code here 
+          string a=to_string(A); 
+        string b=to_string(B);
+        if(A<B)
+            return(a+" is less than "+b);
+        else if(A>B)
+            return(a+" is greater than "+b);
+        else
+            return(a+" is equals to "+b);
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A,B;
+        cin >> A>>B;
+        Solution ob;
+        cout << ob.compareNum(A,B) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
