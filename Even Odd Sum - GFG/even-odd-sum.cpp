102b29f65fe9941e0abd26f086c3b0e946374b08
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2023-12-12 21:36:22 +0530

    Added solution - LeetHub

---
+++ Even Odd Sum - GFG/even-odd-sum.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> EvenOddSum(int N, int Arr[]) {
+        // code here
+        int sumodd=0,sumeven=0;
+       for(int i=0;i<N;i++){
+           if(i%2==0){
+               sumeven += Arr[i];
+           }
+           else{
+               sumodd += Arr[i];
+           }
+       }
+       return {sumeven,sumodd};
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        int Arr[N];
+        for (int i = 0; i < N; i++) cin >> Arr[i];
+        Solution ob;
+        vector<int> ans = ob.EvenOddSum(N, Arr);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+}
+// } Driver Code Ends
