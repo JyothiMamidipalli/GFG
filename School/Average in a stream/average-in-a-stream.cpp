d17feb38cf9b80dd4e10136757a6bd49f03fa7b1
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2024-01-13 22:18:57 +0530

    Added Solution - GfG to GitHub

---
+++ School/Average in a stream/average-in-a-stream.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:	
+	vector<float> streamAvg(int arr[], int n) {
+	    // code here
+	    int i = 0;
+        vector<float>mean;
+        float avg = 0;
+        float sum = 0;
+        while(i<n){
+            sum = sum + arr[i];
+            avg = sum/(i+1);
+            mean.push_back(avg);
+            i++;
+        }
+        return mean;
+	}
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.streamAvg(arr, n);
+        cout << fixed<< setprecision(2);
+        for (auto x : ans) {
+            cout <<x<<" ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
