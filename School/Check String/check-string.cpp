461e7d3784c9e3c300d05bd0cc7eef303b5dd012
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2024-01-14 22:37:54 +0530

    Added Solution - GfG to GitHub

---
+++ School/Check String/check-string.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        bool check (string s)
+        {
+        	//code here.
+        	int i=0;
+             for(int j=1;j<s.length();j++){
+               if(s[i]!=s[j]){
+                   return 0;}
+                }
+            return 1;
+        	
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+		string s;
+		cin >> s;
+		Solution ob;
+		if (ob.check (s))
+			cout << "YES\n";
+		else 
+			cout << "NO\n";
+	}
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
