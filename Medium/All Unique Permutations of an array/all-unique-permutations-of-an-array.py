62dadfa7e3b0bba6eca78b861b8220dada581540
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2024-01-17 22:54:16 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/All Unique Permutations of an array/all-unique-permutations-of-an-array.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+from itertools import permutations
+
+class Solution:
+    def uniquePerms(self, arr, n):
+        # code here 
+        unique_perms = set(permutations(arr))
+        res = [list(perm) for perm in unique_perms]
+        res.sort()
+        return res
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        n=int(input())
+        arr=list(map(int,input().split()))
+        
+        ob = Solution()
+        res = ob.uniquePerms(arr,n)
+        for i in range(len(res)):
+            for j in range(n):
+                print(res[i][j],end=" ")
+            print()
+# } Driver Code Ends
